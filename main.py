7802339ec5e297470fc78c985623e9b0b2ec88fe
Huey-J <wkdgus7113@gmail.com>
2022-08-28 04:56:35 +0900

    Add function to request data_lab api

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
+import requests
+import json
+
+with open('secret.json') as JSON_FILE:
+    SECRET_JSON = json.load(JSON_FILE)
+
+
+def find_datalab_cities():
+    return requests.request(
+        "POST", SECRET_JSON['API_URL']['DATA_LAB']['FIND_GU'],
+        headers={}, data={
+            'sidoCd': SECRET_JSON['API_KEY']['DATA_LAB']['SggCd']
+        }
+    )
+
+
+def find_datalab_pois(arrSggSidoCd):
+    return requests.request(
+        "POST", SECRET_JSON['API_URL']['DATA_LAB']['FIND_PLACE'],
+        headers={}, data={
+            'arrSggCd[]': SECRET_JSON['API_KEY']['DATA_LAB']['SggCd'],
+            'arrSggSidoCd[]': arrSggSidoCd,
+            'txtSGG_CD': '1',
+            'txtSIDO_ARR': '1',
+            'SGG_CD': '11215',
+            'SIDO_ARR': '11215',
+            'BASE_YM1': '202108',
+            'BASE_YM2': '202207',
+            'TMAP_CATE_MCLS_CD': '전체',
+            'srchAreaDate': '1',
+            'qid': 'BDT_03_04_003'
+        }
+    )
+
+
+def find_naverplace_info():
+    # TODO
+    pass
+
+
+def find_naver_place_review():
+    # TODO
+    pass
+
+
+def save_database():
+    # TODO
+    pass
+
+
 def place_crawler(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World!'
+    # 1
+    response = find_datalab_cities()
+    if response.status_code != 200:
+        print("[ERROR] find_datalab_cities")
+        return
+    print(response.txt)
+    # print(response.json()['list'])
+
+    # 2
+    response = find_datalab_pois('11215')
+    if response.status_code != 200:
+        print("[ERROR] find_datalab_pois")
+    print(response.text)
+
+
+place_crawler('')
