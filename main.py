9e99fb919c3e925d427d26f0cbcad61aa0e30b99
huey-J <wkdgus7113@gmail.com>
2022-08-30 10:40:29 +0900

    Add log for api req, res

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ import api.naverplace
 
 # TODO API 예외처리 추가
 def place_crawler(request):
+    print("[REQUEST] DATALAB:FIND_REGIONS >>>")
     region_list = api.datalab.find_regions()
-    print(len(region_list.json()['list']), region_list.text)
+    print("[RESPONSE] DATALAB:FIND_REGIONS >>>", region_list.status_code, region_list.text)
 
     for region in region_list.json()['list']:
-        print(region["sggNm"], region["sggCd"])
+        print("[REQUEST] DATALAB:FIND_POIS >>>", region["sggNm"], region["sggCd"])
         pois_list = api.datalab.find_pois(region["sggCd"])
-        print(len(pois_list.json()['list']), pois_list.text)
+        print("[RESPONSE] DATALAB:FIND_POIS >>>", pois_list.status_code, pois_list.text)
 
         for poi in pois_list.json()['list']:
+            print("[REQUEST] NAVER_PLACE:FIND_DETAIL >>>", poi["ITS_BRO_NM"])
             naver_place = api.naverplace.find_detail(poi["ITS_BRO_NM"])
-            print("@@@@@@@@", naver_place.text)
-            print(naver_place.json()['result']['place']['list'][0])
+            print("[RESPONSE] NAVER_PLACE:FIND_DETAIL >>>", naver_place.status_code, naver_place.text)
+
+            print("[REQUEST] NAVER_PLACE:FIND_REVIEW >>>", naver_place.json()['result']['place']['list'][0]['id'])
             naver_review = api.naverplace.find_review(naver_place.json()['result']['place']['list'][0]['id'])
-            print("@@@@@@@@", naver_review.text)
+            print("[RESPONSE] NAVER_PLACE:FIND_REVIEW >>>", naver_review.status_code, naver_review.text)
+
             print(naver_review.json()[0]['data']['visitorReviewStats'])
 
             # TODO Save Database
