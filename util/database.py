f694f953cdb65a666964166609bccfdb6fe2b1ff
Seongbum Seo <seo@seongbum.com>
2022-09-09 22:15:08 +0900

    Add dataase connector class

---
+++ util/database.py
@@ -0,0 +1,17 @@
+import pymysql
+import configparser
+
+config = configparser.ConfigParser()
+config.read_file(open('config/config.default.ini'))
+
+class Database:
+    def __init__(self):
+        self.session = pymysql.connect(
+            host=config['DATABASE']['HOST'],
+            port=int(config['DATABASE']['PORT']),
+            user=config['DATABASE']['USER'],
+            password=config['DATABASE']['PASSWORD'],
+            db=config['DATABASE']['DB'],
+            charset=config['DATABASE']['CHARSET'],
+        )
+        self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
