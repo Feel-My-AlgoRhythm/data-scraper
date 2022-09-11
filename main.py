107577bd6f8e27415b50aefcdce97c44c662620a
Seongbum Seo <seo@seongbum.com>
2022-09-11 21:16:12 +0900

    Redevelopment main module

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-import api.datalab
-import api.naverplace
+import argparse
+from util.database import Database
 
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
 
-# TODO API 예외처리 추가
-def place_crawler(request):
-    print("[REQUEST] DATALAB:FIND_REGIONS >>>")
-    region_list = api.datalab.find_regions()
-    print("[RESPONSE] DATALAB:FIND_REGIONS >>>", region_list.status_code, region_list.text)
+    arg_parser.add_argument(
+        "--debug", "-d",
+        default=False,
+        action="store_true",
+        help="Run in debug mode.",
+    )
+    arg_parser.add_argument(
+        "--task", "-t",
+        type=str,
+        required=True,
+        choices=["datalab", "naverplace", "review"],
+        help="Task for scraping information.",
+    )
 
-    for region in region_list.json()['list']:
-        print("[REQUEST] DATALAB:FIND_POIS >>>", region["sggNm"], region["sggCd"])
-        pois_list = api.datalab.find_pois(region["sggCd"])
-        print("[RESPONSE] DATALAB:FIND_POIS >>>", pois_list.status_code, pois_list.text)
-
-        for poi in pois_list.json()['list']:
-            print("[REQUEST] NAVER_PLACE:FIND_DETAIL >>>", poi["ITS_BRO_NM"])
-            naver_place = api.naverplace.find_detail(poi["ITS_BRO_NM"])
-            print("[RESPONSE] NAVER_PLACE:FIND_DETAIL >>>", naver_place.status_code, naver_place.text)
-
-            print("[REQUEST] NAVER_PLACE:FIND_REVIEW >>>", naver_place.json()['result']['place']['list'][0]['id'])
-            naver_review = api.naverplace.find_review(naver_place.json()['result']['place']['list'][0]['id'])
-            print("[RESPONSE] NAVER_PLACE:FIND_REVIEW >>>", naver_review.status_code, naver_review.text)
-
-            print(naver_review.json()[0]['data']['visitorReviewStats'])
-
-            # TODO Save Database
-
-
-place_crawler('')
+    args = arg_parser.parse_args()
+    if args.task == "review":
+        from api.review import ReviewParser
+        parser = ReviewParser()
+        result = parser.run()
+        print(f"{result} review data stored.")
